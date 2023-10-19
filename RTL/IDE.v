b31d9414c2ff579fb93a0bb561b4c42d07f5a267
Matt Harlum <matt@harlum.net>
2023-10-19 23:08:03 +0000

    Change IDECS decode, tweak timing
    * Don't latch AS_n on falling edge, setup time not guaranteed
    * IOW now active from S4
    * On Reads, open IDE buffers as soon as UDS/LDS asserted

--- RTL/IDE.v
+++ RTL/IDE.v
@@ -43,7 +43,6 @@ reg ide_enabled = 0;
 reg bank_sel = 0;
 
 reg [1:0] as_delay; // AS_n shifted by CLK
-reg S3_n;           // S3 has started
 
 assign AS_n_S4 = as_delay[0];
 
@@ -53,8 +52,8 @@ always @(posedge CLK or negedge RESET_n) begin
     ROM_BANK <= 0;
   end else begin
     // IDE enabled on first write to an IDE address
-    if (ide_access && ADDR[16:15] == 2'b00 && !RW && !UDS_n && !S3_n) ide_enabled <= 1;
-    if (ide_access && ADDR[16:15] == 2'b01 && !RW && !UDS_n && !S3_n) ROM_BANK <= DIN;
+    if (ide_access && ADDR[16:15] == 2'b00 && !RW && !UDS_n && !as_delay[0]) ide_enabled <= 1;
+    if (ide_access && ADDR[16:15] == 2'b01 && !RW && !UDS_n && !as_delay[0]) ROM_BANK <= DIN;
   end
 end
 
@@ -62,21 +61,13 @@ wire CS_0 = ide_enabled && ide_access && ADDR[16:15] == 2'b00 && ADDR[13:12] == 
 wire CS_1 = ide_enabled && ide_access && ADDR[16:15] == 2'b00 && ADDR[13:12] == 2'b10;
 
 assign IDE1_CS_n[0] = !(!ADDR[14] && CS_0);
-assign IDE1_CS_n[1] = !(!ADDR[14] && CS_1);
-assign IDE2_CS_n[0] = !( ADDR[14] && CS_0);
+assign IDE2_CS_n[0] = !(!ADDR[14] && CS_1);
+assign IDE1_CS_n[1] = !( ADDR[14] && CS_0);
 assign IDE2_CS_n[1] = !( ADDR[14] && CS_1);
 
 // IDE ROM is mapped to whole range until ide is enabled by the first write
 // After then, it is mapped to (base address) + 64K
 assign IDE_ROMEN = !(!AS_n && ide_access && (!ide_enabled || !(ADDR[12] ^ ADDR[13]) || ADDR[16]));
-
-always @(negedge CLK or negedge RESET_n) begin
-  if (!RESET_n) begin
-    S3_n <= 1;
-  end else begin
-    S3_n <= AS_n;
-  end
-end
 
 always @(posedge CLK or negedge RESET_n) begin
   if (!RESET_n) begin
@@ -85,14 +76,14 @@ always @(posedge CLK or negedge RESET_n) begin
     if (AS_n) begin
       as_delay[1:0] <= 2'b11;
     end else begin
-      as_delay <= {as_delay[0], S3_n};
+      as_delay <= {as_delay[0], AS_n};
     end
   end
 end
 
-// IOR Active during states S3-S6
-// IOW Active during states S3-S5
-assign IOR_n = !(!AS_n &&  RW && !S3_n);
-assign IOW_n = !(!AS_n && !RW && !S3_n && as_delay[1]);
+// IOR Active during states S4-S6
+// IOW Active during states S4-S5
+assign IOR_n = !(!AS_n &&  RW && !as_delay[0]);
+assign IOW_n = !(!AS_n && !RW && !as_delay[0] && as_delay[1]);
 
 endmodule
