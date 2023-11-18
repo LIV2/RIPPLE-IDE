1de917e2a81989d0ecfc98cc7a0f6fed97f083b5
Matt Harlum <matt@harlum.net>
2023-11-18 19:36:23 +0000

    Revert timing changes

--- RTL/IDE.v
+++ RTL/IDE.v
@@ -41,6 +41,7 @@ reg ide_enabled = 0;
 
 
 reg [1:0] as_delay; // AS_n shifted by CLK
+reg S3_n;           // S3 has started
 
 assign AS_n_S4 = as_delay[0];
 
@@ -49,7 +50,7 @@ always @(posedge CLK or negedge RESET_n) begin
     ide_enabled <= 0;
   end else begin
     // IDE enabled on first write to an IDE address
-    if (ide_access && ADDR[16:15] == 2'b00 && !RW && !UDS_n && !as_delay[0]) ide_enabled <= 1;
+    if (ide_access && !RW && !UDS_n && !S3_n) ide_enabled <= 1;
   end
 end
 
@@ -65,6 +66,14 @@ assign IDE2_CS_n[1] = !( ADDR[14] && CS_1);
 // After then, it is mapped to (base address) + 64K
 assign IDE_ROMEN = !(!AS_n && ide_access && (!ide_enabled || !(ADDR[12] ^ ADDR[13]) || ADDR[16]));
 
+always @(negedge CLK or negedge RESET_n) begin
+  if (!RESET_n) begin
+    S3_n <= 1;
+  end else begin
+    S3_n <= AS_n;
+  end
+end
+
 always @(posedge CLK or negedge RESET_n) begin
   if (!RESET_n) begin
     as_delay <= 2'b11;
@@ -72,14 +81,14 @@ always @(posedge CLK or negedge RESET_n) begin
     if (AS_n) begin
       as_delay[1:0] <= 2'b11;
     end else begin
-      as_delay <= {as_delay[0], AS_n};
+      as_delay <= {as_delay[0], S3_n};
     end
   end
 end
 
-// IOR Active during states S4-S6
-// IOW Active during states S4-S5
-assign IOR_n = !(!AS_n &&  RW && !as_delay[0]);
-assign IOW_n = !(!AS_n && !RW && !as_delay[0] && as_delay[1]);
+// IOR Active during states S3-S6
+// IOW Active during states S3-S5
+assign IOR_n = !(!AS_n &&  RW && !S3_n);
+assign IOW_n = !(!AS_n && !RW && !S3_n && as_delay[1]);
 
 endmodule
